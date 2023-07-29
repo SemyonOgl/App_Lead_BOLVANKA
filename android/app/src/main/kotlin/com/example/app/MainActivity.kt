e87662833ccc3ec7615091ccfb38843c8f83ddd9
Semyon Ogluzdin <130929023+SemyonOgl@users.noreply.github.com>
2023-07-29 21:48:28 +0300

    Add files via upload

--- android/app/src/main/kotlin/com/example/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/app/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.example.app
 
 import io.flutter.embedding.android.FlutterActivity
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import com.yandex.mapkit.MapKitFactory
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        MapKitFactory.setApiKey("91496832-8348-4295-afa2-6df85ed6b8b2")
+        super.configureFlutterEngine(flutterEngine)
+    }
 }
